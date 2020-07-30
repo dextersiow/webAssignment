6a360b401af01da070574db55c744eccd96b765c
Eugene Ho <Eugene Ho@LAPTOP-VSSLCTOM>
2020-07-31 00:33:34 +0800

    ok

---
+++ checkout.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+         <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="generator" content="Jekyll v4.0.1">
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/starter-template/">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>  
+    
+    <link href="assets/css/bootstrap.css" rel="stylesheet">   
+    <link href="assets/css/starter-template.css" rel="stylesheet">    
+    <script src="assets/javascript/bootstrap.bundle.js"></script></body>
+      
+    <title>CHELL'S FRUIT</title>
+  
+    <style>
+        *{
+            color: orange;
+        }
+        
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    </head>
+    <body>
+         <nav class="navbar navbar-expand-md navbar-light bg-light " style="margin-top: 0">
+        
+    <a class="navbar-brand" href="index.php">CHELL'S FRUITS</a>
+   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+     <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+          <a class="nav-link" href="#">What's NEW<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Promotion</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Admin</a>
+      </li> 
+        <li class="nav-item dropdown active">
+        <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Products</a>
+        <div class="dropdown-menu" aria-labelledby="dropdown01">
+            <a class="dropdown-item" href="#">All</a>
+          <a class="dropdown-item" href="#">Seasonal fruits</a>
+          <a class="dropdown-item" href="#">Stone fruits</a>
+          <a class="dropdown-item" href="#">Berries</a>
+          <a class="dropdown-item" href="#">Tropical and exotic</a>          
+          <a class="dropdown-item" href="#">Others</a>
+        </div>
+        </li>
+        </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+      <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>   
+</nav>
+        
+        <div class="row">
+            
+            <div>
+                
+            </div>
+            
+        </div>
+    </body>
+</html>
