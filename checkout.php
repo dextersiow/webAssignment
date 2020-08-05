ff630d4b19a0b3dde881bfca26d89be8701148ad
Dexter <User@DESKTOP-0L1O0LA>
2020-08-05 22:12:16 +0800

    just eddited

--- checkout.php
+++ checkout.php
@@ -6,80 +6,96 @@ and open the template in the editor.
 -->
 <html>
     <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="generator" content="Jekyll v4.0.1">
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/starter-template/">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>  
-    
-    <link href="assets/css/bootstrap.css" rel="stylesheet">   
-    <link href="assets/css/homepage.css" rel="stylesheet">    
-    <script src="assets/javascript/bootstrap.bundle.js"></script>
-    
-    <style>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="generator" content="Jekyll v4.0.1">
+        <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/starter-template/">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>  
+
+        <link href="assets/css/bootstrap.css" rel="stylesheet">   
+        <link href="assets/css/homepage.css" rel="stylesheet">    
+        <script src="assets/javascript/bootstrap.bundle.js"></script>
+
+        <style>
+            .jumbotron{
+                background-color: orange;
+            }
+            
+            h1{
+                color: white;
+            }
             .cart-itemname{                
                 width: 30%;
             }
-    </style>
-      
-    <title>CHELL'S FRUIT</title>
-    
+            
+            img{
+                height: 5%;
+                width: 35%;
+            }
+        </style>
+
+        <title>CHELL'S FRUIT</title>
+
     </head>
-    
+
     <body>
         <header><a href="index.php"><img src="pics/logo.jpeg" class="mx-auto d-block"> </a></header>
         <div class="container">
-        <div class="jumbotron text-center">
-            <h1>Checkout</h1>
-        </div>
-        <div class="row">
-            <div class="col-6">
-                <form action="#" method="POST">
-                    <label for="contact">Contact Information: </label><br>
-                    <input type="text" name="contact"/><br>
-                    <label for="delivery">Delivery Method: </label><br>
-                    <table class="table table-bordered">
-                        <tr>
-                            <td><input type="radio" name="delivery" value="ship">Ship<br></td>
-                            <td><input type="radio" name="delivery" value="pickup">Pick up<br></td>
-                        </tr>
-                    </table>
-                    <label for="shipping">Shipping Address: </label><br>
-                    <table>
-                        <tr><td>First Name: <input type="text" name="fname"></td><td>Last Name: <input type="text" name="lname"></td></tr>
-                        <tr><td colspan="2">Address: <input type="text" name="address" size="60px"></td></tr>
-                        <tr><td colspan="2">Phone Number: <input type="tel" name="phone" size="53px"></td></tr>  
-                        <tr><td colspan="2"><input type="submit" value="Continue to shipping"></td></tr>
-                    </table>
-                 </form>
+            <div class="jumbotron text-center">
+                <h1>Checkout</h1>
             </div>
-            <div class="col-6 bg-light">
-                 <table class="table table-hover table" style="border-bottom:1px;">  
-                    <tr class="cart cart-row">
-                        <td width="55%"><img src="./pics/products/australia_carrot.jpg"></td>
-                        <td>Australia Carrot</td>
-                        <td>25.00</td>
-                        <td>1</td>
-                        <td>25.00</td>                        
-                    </tr>   
-                <tfoot>
-                    <tr>
-                        <td>Subtotal:<br>Shipping:</td>
-                        <td colspan="4" style="text-align:right">RM 25.00<br>RM 10.00</td>
-                    </tr>
-                    <tr>
-                        <td>Total:</td>
-                        <td colspan="4" style="text-align: right">RM 35.00<br></td>
-                    </tr>
-                </tfoot>
-            </table>
+            <div class="row">
+                <div class="col-6">               
+                    <form action="completeOrder.php" method="POST">
+                        <table class="table table-borderless">
+                            <tr>
+                                <td><label for="contact">Contact Information: </label><br>
+                                <input type="text" name="contact"/><br></td>
+                            </tr>
+                            <tr>
+                                <td><label for="delivery">Delivery Method: </label></td> 
+                            </tr>
+                            <tr>
+                                <td><input type="radio" name="delivery" value="ship">Ship</td>
+                                <td><input type="radio" name="delivery" value="pickup">Pick up</td>
+                            </tr>
+                        
+                            <tr><td><label for="shipping">Shipping Address: </label></td></tr>
+                       
+                            <tr><td>First Name: <input type="text" name="fname"></td><td>Last Name: <input type="text" name="lname"></td></tr>
+                            <tr><td colspan="2">Address: <input type="text" name="address" size="60px"></td></tr>
+                            <tr><td colspan="2">Phone Number: <input type="tel" name="phone" size="53px"></td></tr>  
+                            <tr><td colspan="2"><input type="submit" value="Complete Order"></td></tr>
+                        </table>
+                    </form>
+                </div>
+                <div class="col-6 bg-light">
+                    <table class="table table-hover" style="border-bottom:1px;">  
+                        <tr class="cart cart-row">
+                            <td class="cart-itemimg"><img src="pics/products/australia_carrot.jpg"></td>
+                            <td class="cart-itemname">Australia Carrot</td>
+                            <td class="item-price">25.00</td>                        
+                            <td class="item-quantity">1</td>
+                            <td class="item-total">25.00</td>                       
+                        </tr>   
+                        <tfoot>
+                            <tr>
+                                <td>Subtotal:<br>Shipping:</td>
+                                <td colspan="4" style="text-align:right">RM 25.00<br>RM 10.00</td>
+                            </tr>
+                            <tr>
+                                <td>Total:</td>
+                                <td colspan="4" style="text-align: right">RM 35.00<br></td>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </div>
             </div>
         </div>
-        </div>
-         <?php include 'footer.php'; ?>       
-       
+        <?php include 'footer.php'; ?>       
+
     </body>
 </html>
--- product.php
+++ product.php
@@ -105,174 +105,117 @@ and open the template in the editor.
             <div class="row">
                 <div class="col-3">
                     <div class="product-top">
-                        <img class="product-img" src="./pics/products/aiwen_mango.jpg">
+                        <img class="product-img" src="./pics/products/australia_rock_melon.jpg">
                         <div class="overlay">
                             <button type="button" class="btn btn-secondary" title="Preview"><i class="fa fa-eye"></i></button>
                             <button type="button" class="btn btn-secondary" title="Add to cart"><i class="fa fa-shopping-cart"></i></button>                        
                         </div>
                     </div>
                     <div class="product-bottom text-center">
-                        <h3>Aiwen Mango</h3>
-                        <h5>RM 10.00</h5>
+                        <h3>Australia Rock Melon</h3>
+                        <h5>RM 12.00</h5>
                     </div>
                 </div>
                 
                 <div class="col-3">
                     <div class="product-top">
-                        <img class="product-img" src="./pics/products/australia_carrot.jpg">
+                        <img class="product-img" src="./pics/products/rambutan.jpg">
                         <div class="overlay">
                             <button type="button" class="btn btn-secondary" title="Preview"><i class="fa fa-eye"></i></button>
                             <button type="button" class="btn btn-secondary" title="Add to cart"><i class="fa fa-shopping-cart"></i></button>                        
                         </div>
                     </div>
                     <div class="product-bottom text-center">
-                        <h3>Australia Carrot</h3>
-                        <h5>RM 20.00</h5>
+                        <h3>rambutan</h3>
+                        <h5>RM 15.00</h5>
                     </div>
                 </div>
                 
                 <div class="col-3">
                     <div class="product-top">
-                        <img class="product-img" src="pics/products/golden_dragon_mango.jpg">
+                        <img class="product-img" src="pics/products/young_coconut.jpg">
                         <div class="overlay">
                             <button type="button" class="btn btn-secondary" title="Preview"><i class="fa fa-eye"></i></button>
                             <button type="button" class="btn btn-secondary" title="Add to cart"><i class="fa fa-shopping-cart"></i></button>                        
                         </div>
                     </div>
                     <div class="product-bottom text-center">
-                        <h3>Golden Dragon Mango</h3>
-                        <h5>RM 18.00</h5>
+                        <h3>Young Coconut</h3>
+                        <h5>RM 6.00</h5>
                     </div>            
                 </div>
                 
                 <div class="col-3">
                     <div class="product-top">
-                        <img class="product-img" src="pics/products/forelle_pear.jpg">
+                        <img class="product-img" src="pics/products/green_grapes.jpg">
                         <div class="overlay">
                             <button type="button" class="btn btn-secondary" title="Preview"><i class="fa fa-eye"></i></button>
                             <button type="button" class="btn btn-secondary" title="Add to cart"><i class="fa fa-shopping-cart"></i></button>                        
                         </div>
                     </div>
                     <div class="product-bottom text-center">
-                        <h3>Forelle Pear</h3>
-                        <h5>RM 11.00</h5>
+                        <h3>Green Grapes</h3>
+                        <h5>RM 17.00</h5>
                     </div>
                 </div>
             </div>
             <div class="row">
                 <div class="col-3">
                     <div class="product-top">
-                        <img class="product-img" src="./pics/products/aiwen_mango.jpg">
+                        <img class="product-img" src="./pics/products/thai_lychee.jpg">
                         <div class="overlay">
                             <button type="button" class="btn btn-secondary" title="Preview"><i class="fa fa-eye"></i></button>
                             <button type="button" class="btn btn-secondary" title="Add to cart"><i class="fa fa-shopping-cart"></i></button>                        
                         </div>
                     </div>
                     <div class="product-bottom text-center">
-                        <h3>Aiwen Mango</h3>
-                        <h5>RM 10.00</h5>
-                    </div>
-                </div>
-                
-                <div class="col-3">
-                    <div class="product-top">
-                        <img class="product-img" src="./pics/products/australia_carrot.jpg">
-                        <div class="overlay">
-                            <button type="button" class="btn btn-secondary" title="Preview"><i class="fa fa-eye"></i></button>
-                            <button type="button" class="btn btn-secondary" title="Add to cart"><i class="fa fa-shopping-cart"></i></button>                        
-                        </div>
-                    </div>
-                    <div class="product-bottom text-center">
-                        <h3>Australia Carrot</h3>
-                        <h5>RM 20.00</h5>
-                    </div>
-                </div>
-                
-                <div class="col-3">
-                    <div class="product-top">
-                        <img class="product-img" src="pics/products/golden_dragon_mango.jpg">
-                        <div class="overlay">
-                            <button type="button" class="btn btn-secondary" title="Preview"><i class="fa fa-eye"></i></button>
-                            <button type="button" class="btn btn-secondary" title="Add to cart"><i class="fa fa-shopping-cart"></i></button>                        
-                        </div>
-                    </div>
-                    <div class="product-bottom text-center">
-                        <h3>Golden Dragon Mango</h3>
+                        <h3>Thai Lychee</h3>
                         <h5>RM 18.00</h5>
+                    </div>
+                </div>
+                
+                <div class="col-3">
+                    <div class="product-top">
+                        <img class="product-img" src="./pics/products/red_seedless_watermelon.jpg">
+                        <div class="overlay">
+                            <button type="button" class="btn btn-secondary" title="Preview"><i class="fa fa-eye"></i></button>
+                            <button type="button" class="btn btn-secondary" title="Add to cart"><i class="fa fa-shopping-cart"></i></button>                        
+                        </div>
+                    </div>
+                    <div class="product-bottom text-center">
+                        <h3>Red Seedless Watermelon</h3>
+                        <h5>RM 18.00</h5>
+                    </div>
+                </div>
+                
+                <div class="col-3">
+                    <div class="product-top">
+                        <img class="product-img" src="pics/products/beetroot.jpg">
+                        <div class="overlay">
+                            <button type="button" class="btn btn-secondary" title="Preview"><i class="fa fa-eye"></i></button>
+                            <button type="button" class="btn btn-secondary" title="Add to cart"><i class="fa fa-shopping-cart"></i></button>                        
+                        </div>
+                    </div>
+                    <div class="product-bottom text-center">
+                        <h3>Beetroot</h3>
+                        <h5>RM 9.00</h5>
                     </div>            
                 </div>
                 
                 <div class="col-3">
                     <div class="product-top">
-                        <img class="product-img" src="pics/products/forelle_pear.jpg">
+                        <img class="product-img" src="pics/products/tomatoes.jpg">
                         <div class="overlay">
                             <button type="button" class="btn btn-secondary" title="Preview"><i class="fa fa-eye"></i></button>
                             <button type="button" class="btn btn-secondary" title="Add to cart"><i class="fa fa-shopping-cart"></i></button>                        
                         </div>
                     </div>
                     <div class="product-bottom text-center">
-                        <h3>Forelle Pear</h3>
-                        <h5>RM 11.00</h5>
+                        <h3>Tomato</h3>
+                        <h5>RM 3</h5>
                     </div>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col-3">
-                    <div class="product-top">
-                        <img class="product-img" src="./pics/products/aiwen_mango.jpg">
-                        <div class="overlay">
-                            <button type="button" class="btn btn-secondary" title="Preview"><i class="fa fa-eye"></i></button>
-                            <button type="button" class="btn btn-secondary" title="Add to cart"><i class="fa fa-shopping-cart"></i></button>                        
-                        </div>
-                    </div>
-                    <div class="product-bottom text-center">
-                        <h3>Aiwen Mango</h3>
-                        <h5>RM 10.00</h5>
-                    </div>
-                </div>
-                
-                <div class="col-3">
-                    <div class="product-top">
-                        <img class="product-img" src="./pics/products/australia_carrot.jpg">
-                        <div class="overlay">
-                            <button type="button" class="btn btn-secondary" title="Preview"><i class="fa fa-eye"></i></button>
-                            <button type="button" class="btn btn-secondary" title="Add to cart"><i class="fa fa-shopping-cart"></i></button>                        
-                        </div>
-                    </div>
-                    <div class="product-bottom text-center">
-                        <h3>Australia Carrot</h3>
-                        <h5>RM 20.00</h5>
-                    </div>
-                </div>
-                
-                <div class="col-3">
-                    <div class="product-top">
-                        <img class="product-img" src="pics/products/golden_dragon_mango.jpg">
-                        <div class="overlay">
-                            <button type="button" class="btn btn-secondary" title="Preview"><i class="fa fa-eye"></i></button>
-                            <button type="button" class="btn btn-secondary" title="Add to cart"><i class="fa fa-shopping-cart"></i></button>                        
-                        </div>
-                    </div>
-                    <div class="product-bottom text-center">
-                        <h3>Golden Dragon Mango</h3>
-                        <h5>RM 18.00</h5>
-                    </div>            
-                </div>
-                
-                <div class="col-3">
-                    <div class="product-top">
-                        <img class="product-img" src="pics/products/forelle_pear.jpg">
-                        <div class="overlay">
-                            <button type="button" class="btn btn-secondary" title="Preview"><i class="fa fa-eye"></i></button>
-                            <button type="button" class="btn btn-secondary" title="Add to cart"><i class="fa fa-shopping-cart"></i></button>                        
-                        </div>
-                    </div>
-                    <div class="product-bottom text-center">
-                        <h3>Forelle Pear</h3>
-                        <h5>RM 11.00</h5>
-                    </div>
-                </div>
-            </div>
+            </div>            
         </div>
         <?php include'footer.php' ?>
     </body>
