37c154160818cbb7b6ee76c5a59654e6a14701e8
Eugene Ho <Eugene Ho@LAPTOP-VSSLCTOM>
2020-08-27 01:31:24 +0800

    viewOrder done

--- updateStatus.php
+++ updateStatus.php
@@ -1,10 +1,101 @@
-
-<?php
-     $selected_option=$_POST['option_value'];
-
-     //Do what you need with this value, then echo what you want to be returned.
-
-     echo "you have selected the option value=$selected_option";
-
+<?php 
+session_start();
+if (empty($_SESSION['loggedin'])){
+    header('Location: index.php');
+}
 ?>
 
+<html>
+    <head>
+        <?php include 'links.php'?>
+        <title>CHELL'S FRUIT</title>
+        
+       
+    </head>
+    <body>
+        <?php
+        require_once 'config.php';
+        
+        $orderID = $_REQUEST['oid'];
+        if(isset($_POST['submit'])){
+            $pStatus = $_POST['payment'];
+            $dStatus = $_POST['delivery'];
+           
+            $sql2 = "UPDATE fruit_order SET payment_status='{$pStatus}',delivery_status='{$dStatus}' where order_id='{$orderID}'";
+            if(mysqli_query($link, $sql2)){
+                header("Location: view_orders.php");                   
+            }
+            else{
+                $message = "Update failed, Please try again later";
+                }
+        }
+        
+        $sql = "SELECT * from fruit_order where order_id='{$orderID}'";
+        $result = mysqli_query($link, $sql);
+        $row=$result->fetch_assoc();
+        extract($row);
+     
+        mysqli_close($link);    
+        ?>       
+        
+        <div class="container" style="margin-left: 10px;">
+            <form action='updateStatus.php?oid=<?php echo $orderID;?>' method='post' class="needs-validation" novalidate enctype="multipart/form-data">
+            <h2>Edit product</h2>
+            <div>
+                <a href="view_orders.php"><img src="pics/back_btn.png" style="width:1.5%;"></a>
+            </div>
+            <table class="table table-borderless" width='50%'>  
+                <tr>
+                    <td><label for="orderID">Order ID:</label></td>
+                    <td><input type="text" name="orderID" class="form-control" value="<?php echo $row['order_id'];?>" disabled></td>
+                </tr>
+                <tr>
+                    <td><label for="Member ID">Member ID:</label></td>
+                    <td><input type="text" name="memberID" class="form-control" value="<?php echo $row['member_id'];?>" disabled></td>
+                </tr>  
+                <tr>
+                    <td><label for="date">Order Date:</label></td>
+                    <td><input type="text" name="date" class="form-control" value="<?php echo $row['order_date'];?>" disabled></td>
+                </tr> 
+                <tr>
+                    <td><label for="amount">Price:</label></td>
+                    <td><input type="text" name="amount" class="form-control" value="<?php echo 'RM '.(number_format((float)$row['amount'],2,'.',''));?>" disabled></td>    
+                </tr>            
+           
+                <tr>
+                    <td><label for="payment">Payment Status:</label></td>
+                <td>
+                    <select name='payment' class="form-control" required="required">
+                        <option <?php if(strcmp($row['payment_status'],"Pending")==0){echo'selected';}?> value='Pending'>Pending</option>
+                        <option <?php if(strcmp($row['payment_status'],"Received")==0){echo'selected';}?> value='Received'>Received</option>
+                        <option <?php if(strcmp($row['payment_status'],"Refunded")==0){echo'selected';}?> value='Refunded'>Refunded</option>
+                </select>
+                </td>
+                <tr>
+                    <td width='20%'><label for="delivery">Delivery Status:</label></td>
+                <td width='30%'>
+                    <select name='delivery' class="form-control" required="required">
+                        <option <?php if(strcmp($row['delivery_status'],"Preparing")==0){echo'selected';}?> value='Preparing'>Preparing</option>
+                        <option <?php if(strcmp($row['delivery_status'],"Delivering")==0){echo'selected';}?> value='Delivering'>Delivering</option>
+                        <option <?php if(strcmp($row['delivery_status'],"Delivered")==0){echo'selected';}?> value='Delivered'>Delivered</option>
+                        <option <?php if(strcmp($row['delivery_status'],"Cancelled")==0){echo'selected';}?> value='Cancelled'>Cancelled</option>
+                </select>
+                </td>
+                <td width='50%'></td>
+                </tr>            
+                                  
+                </table>
+            
+            <?php if(!empty($message)){
+                echo'<p>'.$message.'</p>';                             
+            }
+                ?>
+            <input type="submit" name="submit" class="btn btn-primary">
+            <input type="button" name="cancel" value="Cancel" class="btn btn-danger" onclick="window.location.href='view_orders.php'">
+            
+         </form>
+        </div>
+        
+    </body>
+    
+</html>
--- view_orders.php
+++ view_orders.php
@@ -4,44 +4,12 @@ if(empty($_SESSION['loggedin'])){
     header('Location: index.php');
 }
 
-if(isset($_POST['submit'])){
-    
-    $pstatus = $_POST['paymentDropdown'];
-    $dstatus = $_POST['deliveryDropdown'];
-    
-    require_once 'config.php';
-
-    $sql="UPDATE fruit_order SET paymentStatus='{$pstatus}', deliveryStatus='{$dstatus} WHERE order_id={$row['order_id']}";
-    if(mysqli_query($link, $sql)){
-        header("Refresh:0");
-    }
-    else{
-        $message = "Error";
-        echo "<script type='text/javascript'>alert('$message');</script>";
-    }
-
-}
 ?>
 <html>
     <head>
         <?php include'links.php'; ?>
         <title>CHELL'S FRUIT</title>   
-        <script>
-            $(document).ready(function(){
-                // everything here will be executed once index.html has finished loading, so at the start when the user is yet to do anything.
-                $("#payment").change(load_new_content()); //this translates to: "when the element with id='select1' changes its value execute load_new_content() function"
-            });
-            function load_new_content(){
-                var selected_option_value=$("#payment option:selected").val(); //get the value of the current selected option.
-
-                $.post("updateStatus.php", {option_value: selected_option_value},
-                function(data){ //this will be executed once the `script_that_receives_value.php` ends its execution, `data` contains everything said script echoed.
-                    $("#place_where_you_want_the_new_html").html(data);
-                    alert(data); //just to see what it returns
-                    }
-                );
-            } 
-         </script>
+        
     </head>
     <body>
         <?php
@@ -58,7 +26,7 @@ if(isset($_POST['submit'])){
         </div>        
         <table class="table" style="width:80%">
             <thead>
-                <tr><th>Order ID</th><th>Member ID</th><th>Order Date</th><th>Order Amount</th><th>Payment Status</th><th>Delivery Status</th><th>Action</th></tr>
+                <tr><th>Order ID</th><th>Member ID</th><th>Order Date</th><th>Order Amount</th><th>Payment Status</th><th>Delivery Status</th><th width='15%'>Action</th></tr>
             </thead>
             <tbody>   
           <?php
@@ -70,23 +38,10 @@ if(isset($_POST['submit'])){
                     ."<td>{$row['member_id']}</td>"
                     ."<td>{$row['order_date']}</td>"
                     ."<td>","RM ",number_format((float)$row['amount'],2,'.',''),"</td>"
-                    ."<td>"
-                        ."<select name='payment' id='payment' class='form-control' onchange='load_new_content()'>"
-                            .'<option value="Pending" '.(($row['payment_status']=='Pending')?'selected':"").'>Pending</option>'
-                            .'<option value="Received" '.(($row['payment_status']=='Received')?'selected':"").'>Received</option>'
-                            .'<option value="Refunded" '.(($row['payment_status']=='Refunded')?'selected':"").'>Refunded</option>'
-                        ."</select></td>"    
-                    ."<td>"
-                        ."<select name='delivery' id='delivery' class='form-control' onchange='load_new_content()'>"
-                            .'<option value="Preparing" '.(($row['delivery_status']=='Preparing')?'selected':"").'>Preparing</option>'
-                            .'<option value="Delivering" '.(($row['delivery_status']=='Delivering')?'selected':"").'>Delivering</option>'
-                            .'<option value="Delivered" '.(($row['delivery_status']=='Delivered')?'selected':"").'>Delivered</option>'
-                            .'<option value="Cancelled" '.(($row['delivery_status']=='Cancelled')?'selected':"").'>Cancelled</option>'
-                        ."</select></td>"
-                    ."<td><a href=updateStatus?ps={$row['payment_status']}&&ds={$row['delivery_status']}.php><img src='./pics/save_btn.ico' width='10px' height='10px'><a></td></tr>";
-                    
-            }
-            
+                    ."<td>{$row['payment_status']}</td>"    
+                    ."<td>{$row['delivery_status']}</td>"                        
+                    ."<td><a href='updateStatus.php?oid={$row['order_id']}'><img src=\"./pics/edit_btn.png\" width='15px' height='15px'><a></td></tr>";
+            }            
             ?>
             </tbody></table>
         </div>
